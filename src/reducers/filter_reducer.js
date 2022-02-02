36c2038006f3e3927fe4e74812fe57fca12b5ade
Catalin Mihai <mishu_leu15@yahoo.com>
2022-02-02 23:01:49 +0200

    added filter functionality, cart context setup and card reducer for adding item to the cart

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -72,10 +72,12 @@ const filter_reducer = (state, action) => {
 
     return { ...state, filtered_products: tempProducts };
   }
+
   if (action.type === UPDATE_FILTERS) {
     const { name, value } = action.payload;
     return { ...state, filters: { ...state.filters, [name]: value } };
   }
+
   if (action.type === FILTER_PRODUCTS) {
     const { all_products } = state;
     const { text, category, company, color, price, shipping } = state.filters;
