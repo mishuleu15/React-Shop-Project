6748be8e635cd530a0c406197e4c473f4d64fd96
Catalin Mihai <mishu_leu15@yahoo.com>
2022-02-03 22:55:41 +0200

    added local storage to cart context, cart page and cart functionality

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -18,7 +18,7 @@ const getLocalStorage = () => {
 };
 
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -33,8 +33,34 @@ export const CartProvider = ({ children }) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
 
+  // remove item
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: id });
+  };
+  // toggle amount
+  const toggleAmount = (id, value) => {
+    dispatch({
+      type: TOGGLE_CART_ITEM_AMOUNT,
+      payload: {
+        id,
+        value,
+      },
+    });
+  };
+  // clear cart
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart));
+    dispatch({ type: COUNT_CART_TOTALS });
+  }, [state.cart]);
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
